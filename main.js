3497664df0ff7ac12b0fd95b88812ed9f0133627
aryaSuresh9876 <66162119+aryaSuresh9876@users.noreply.github.com>
2023-06-20 13:17:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+/*btns = document.querySelectorAll('.btn');
+btns.forEach(function(btn){
+    input = document.getElementById('input');
+    btn.addEventListener('click', function(e){
+        const class_list = e.currentTarget.classList;
+        
+    });
+});*/
+
+        
+
